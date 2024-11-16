90f9c5d44e0d147c562f09feef2729a751553821
Chi <Chi12138@outlook.com>
2024-11-16 14:35:02 +0800

    完成用户的注册和删除

--- my_blog/userprofile/forms.py
+++ my_blog/userprofile/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 # 导入User模型
 from django.contrib.auth.models import User
@@ -8,3 +9,20 @@ from django.contrib.auth.models import User
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
+
+
+class UserRegisterForm(UserCreationForm):
+    # 复写User的密码
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data.get('password1') == data.get('password2'):
+            return data.get('password1')
+        else:
+            raise forms.ValidationError("密码输入不一致,请重试。")
